374f7738b92fe13b69dd4d28ecfc5c24c1943ca4
Sidharth <sid250581@gmail.com>
2024-12-03 00:33:15 +0530

    nitial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import {TodoWrapper} from "./components/todoWrapper"
+//import {TodoForm} from "./components/TodoForm"
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper/>
+      
+    </div>
+  );
+}
+
+export default App;
